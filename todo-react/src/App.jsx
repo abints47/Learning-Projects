64c95d64cdae3326358559b8fb2880f1fcdf66c4
Abin T S <130057270+abints47@users.noreply.github.com>
2026-07-31 17:39:36 +0530

    Update App.jsx

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,12 +1,7 @@
 
 
 function hello(){
-  return (
-    <div>
+  return 
     <h1>hello world</h1>
-    <p>Here is the another Paragrphs..</p>
-    </div>     
-         
-    );
 }
 export default hello;
